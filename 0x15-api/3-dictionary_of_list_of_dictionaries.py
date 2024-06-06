89ee897186e8c2b6804c7b72031edfb1d401eadb
Godwin Essien <kinggodwin4u@yahoo,com>
2024-06-06 01:02:04 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/todos/1"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
