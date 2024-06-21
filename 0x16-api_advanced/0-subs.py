bd738b62f017fa0ee0510125bbd6f85c19271d2d
Godwin Essien <kinggodwin4u@yahoo,com>
2024-06-21 11:56:40 +0100

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,17 @@
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
     """
     define numbers of subscribers
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
+    headers = {'User-Agent': 'Mozilla/5.0 '}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
